5554db3db68599046d191d97cda07575cfbf3dd3
tsasagawa0510 <mail@tsubasa-sasagawa.com>
2023-10-04 13:33:23 +0900

    first commit

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+
+$(function() {
+  
+
+  $(window).on('load', () => {
+    console.log('loadイベントが発生しました。');
+  });
+
+  $(window).on('scroll', () => {
+    console.log('scrollイベントが発生しました。');
+  });
+});
